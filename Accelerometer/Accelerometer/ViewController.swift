0bb936dc0034ca3782121124d4385b133b5f522f
Marcus Eckert <marcuseckert@gmail.com>
2017-07-23 05:44:27 +0200

    bug fix accelerometer example

--- Accelerometer/Accelerometer/ViewController.swift
+++ Accelerometer/Accelerometer/ViewController.swift
@@ -18,7 +18,10 @@ class ViewController: UIViewController {
         self.view.layer.addSublayer(animation!)
         self.view.backgroundColor = UIColor(red: 101.0/255.0, green: 44.0/255.0, blue: 218.0/255.0, alpha: 1.0)
         
-        let layer = animation!.getLayersWithName("Expression Controls")![0]
+        //We're getting the Squall Controls layer from the comp
+        //If you are rendering the animation from AE, make sure the name here matches the names of the null in After Effects.
+        //Converting the project to a newer AE version can sometimes alter the names.
+        let layer = animation!.getLayersWithName("Squall Controls")![0]
         
         //Our Expression Controller does most of the heavy lifting
         let controls = ExpressionController(frame: self.view.frame)
